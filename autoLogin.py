83e5047239a58e43e2315150625891ac5756f9e3
Reza Latifi <reza.bubba@gmail.com>
2019-12-29 23:05:58 +0330

    Update autoLogin.py

--- autoLogin.py
+++ autoLogin.py
@@ -4,45 +4,132 @@ import hashlib
 import os
 import sys
 import socket
+from getpass import getpass
 
-def logIn(data : dict):
-  global hotspotURL
-  if (requests.post(hotspotURL, data).status_code != 200):
-    printError(r.status_code)
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+
+def logIn() -> None:
+  global loginURL
+  global data
+  try:
+    if (requests.post(loginURL, data).status_code != 200):
+      printError(r.status_code)
+      exit()
+  except:
+    print('No network available')
     exit()
 
-def kickUser(data : dict):
+def logOut() -> None:
+  global logOutURL
+  try:
+    if (requests.get(logOutURL).status_code != 200):
+      printError(r.status_code)
+      exit()
+  except:
+    print('No network available')
+    exit()
+
+def kickUser() -> None:
   global vpnReportLoginURL
   global vpnReportKillURL
+  global data
   session = requests.session()
   r = session.post(vpnReportLoginURL, data)
   doc = BeautifulSoup(str(r.content), 'html.parser')
-  data['ras_ip'] = doc.find('input', {'name' : 'ras_ip'})['value']
-  data['unique_id_val'] = doc.find('input', {'name' : 'unique_id_val'})['value']
+  try:
+    data['ras_ip'] = doc.find('input', {'name' : 'ras_ip'})['value']
+    data['unique_id_val'] = doc.find('input', {'name' : 'unique_id_val'})['value']
+  except:
+    print('Invalid ID or Password')
+    exit()
   session.post(vpnReportKillURL, data)
 
-def ping():
+def ping() -> int:
   global pingURL
-  return os.system("ping -c 1 {}".format(pingURL))
+  return os.system('ping -c 1 {} >/dev/null 2>&1'.format(pingURL))
 
-def printError(errorCode : int):
-  print("Connection failed, Code:{}".format(errorCode))
+def printError(errorCode : int) -> None:
+  print('Connection failed, Code:{}'.format(errorCode))
 
+def doLogOut() -> None:
+  while ping() == 0:
+    logOut()
+  print("Disconnected!")
 
-# User dependent variables
-studentId = 1234567890
-password = 'password'
+def doLogIn() -> None:
+  logIn()
+  while ping() != 0:
+    kickUser()
+    logIn()
+  print("Connected!")
+
+def init() -> None:
+  global studentId
+  global password
+  global data
+  global workingDir
+  
+  try: # Reading configurations
+    with open(workingDir+'/.conf.txt', 'r', encoding='utf-8') as config:
+      for line in config:
+        if line.startswith('ID: '):
+          studentId = line[len('ID: '):].rstrip('\n')
+        if line.startswith('Password: '):
+          password = line[len('Password: '):].rstrip('\n')
+
+    data = {'username': studentId, 'normal_username': studentId,
+          'password': hashlib.md5(password.encode("utf-8")).hexdigest(),
+          'normal_password': password, 'kill_me': 1}
+  except: # Configurations not found
+    print("Configuration not found, config now? (y/n)")
+    while True:
+      answer = input()
+      if answer == 'y' or answer == 'yes':
+        doConfig()
+        break
+      if answer == 'n' or answer == 'no':
+        break
+
+def doConfig() -> None:
+  global studentId
+  global password
+  global workingDir
+  while True:
+    studentId = input('Hotspot ID: ')
+    password = getpass('Password: ')
+    if type(studentId) == type(password) == str:
+      config = open(workingDir+'/.conf.txt', 'w')
+      config.write('ID: '+studentId+"\n")
+      config.write('Password: '+password+"\n")
+      print('Done!')
+      break
+    else:
+      print('Invalid input')
+
+# User variables
+studentId = None
+password = None
 
 # These URLs may change in future
-hotspotURL = 'https://hotspot.um.ac.ir/login'
+loginURL = 'https://hotspot.um.ac.ir/login'
+logOutURL = 'http://hotspot.um.ac.ir/logout'
 vpnReportLoginURL = 'https://vpn-report.um.ac.ir/IBSng/user/'
 vpnReportKillURL = 'https://vpn-report.um.ac.ir/IBSng/user/home.php'
 pingURL = 'google.com'
 
 # All parameters needed
-data = {'username': studentId, 'normal_username': studentId,
-        'password': hashlib.md5('ramzenet'.encode("utf-8")).hexdigest(),
-        'normal_password': password, 'kill_me': 1}
+data = None
+workingDir = os.path.dirname(os.path.realpath(__file__))
 
 # Other parameters that forms send but they seem unnecessary
 # data['lang'] = 'en'
@@ -51,8 +138,26 @@ data = {'username': studentId, 'normal_username': studentId,
 # data['dst'] = ''
 # data['popup'] = True
 
-logIn(data)
-while (ping() != 0):
-  kickUser(data)
-  logIn(data)
-print("Connected!")
+if __name__ == "__main__":
+  # Initialize needed arguments
+  init()
+  # Commands
+  if len(sys.argv) > 1:
+    if sys.argv[1] == '-c' or sys.argv[1] == '--connect': # Connect
+      doLogIn()
+    elif sys.argv[1] == '-cnf' or sys.argv[1] == '--config': # Configure
+      doConfig()
+    elif sys.argv[1] == '-d' or sys.argv[1] == '--disconnect': # Disconnect
+      doLogOut()
+    elif sys.argv[1] == '-h' or sys.argv[1] == '--help': # Help
+      print(color.DARKCYAN+color.BOLD+'Ferdowsi University of Mashhad Hotspot Network Auto login.'+color.END+color.END)
+      print(color.UNDERLINE+'Written by Reza Latifi'+color.END)
+      print(color.GREEN+'    Default'+color.END+'\n\t    Connect to the Internet.')
+      print(color.GREEN+'    (-c | --connect)'+color.END+'\n\t    Connect to the Internet.')
+      print(color.GREEN+'    (-d | --disconnect)'+color.END+'\n\t    Disconnect from the Internet.')
+      print(color.GREEN+'    (-cnf | --config)'+color.END+'\n\t    Configure user authentication.')
+      print(color.GREEN+'    (-h | --help)'+color.END+'\n\t    User manual.')  
+    else:
+      print('Command not found try "--help"')
+  else: # Default
+    doLogIn()
